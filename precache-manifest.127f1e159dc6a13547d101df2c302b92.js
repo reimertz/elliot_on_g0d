4ca69fdd204e2638fc5c92e053c0852eedf0c4ac
Piérre Reimertz <reimertz@me.co>
2019-10-28 15:25:30 +0530

    Updates

---
+++ precache-manifest.127f1e159dc6a13547d101df2c302b92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25752d6c3b77c137e9990b89ff32b590",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e530122dab1f0eb2bcdb",
+    "url": "/static/css/main.468cbbd8.chunk.css"
+  },
+  {
+    "revision": "27d9ce4c36c42e256229",
+    "url": "/static/js/2.eef17285.chunk.js"
+  },
+  {
+    "revision": "e530122dab1f0eb2bcdb",
+    "url": "/static/js/main.8f6fbfd1.chunk.js"
+  },
+  {
+    "revision": "4ac5fcb7dd6c01ea84d5",
+    "url": "/static/js/runtime-main.a8c9eda2.js"
+  },
+  {
+    "revision": "832ef8bf3706e7b9b1d277e42e3d5a69",
+    "url": "/static/media/elliot_on_god.832ef8bf.gif"
+  },
+  {
+    "revision": "d5aa790ac72d8bf10643e2139624f16c",
+    "url": "/static/media/logo.d5aa790a.png"
+  }
+]);
